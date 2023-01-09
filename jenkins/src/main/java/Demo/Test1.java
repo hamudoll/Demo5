42f22e2dcc27fcbb74c4499a111749d399d102a3
Dell <Dell@DESKTOP-STMKM98>
2023-01-09 15:58:54 +0530

    first commit

---
+++ jenkins/src/main/java/Demo/Test1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void m1()
+    {
+    	Reporter.log("Hello",true);
+    }
+}
