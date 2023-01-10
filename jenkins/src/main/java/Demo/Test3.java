8423fb21b37e413bb8708183d1705d8292ecce6a
Dell <Dell@DESKTOP-STMKM98>
2023-01-10 15:41:46 +0530

    fourth commit

---
+++ jenkins/src/main/java/Demo/Test3.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	 @Test
+	    public void m5()
+	    {
+	    	Reporter.log("Hello ji");
+	    }
+}
